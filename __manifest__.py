ce86e89f9784e3b393fd5705449a68fb217832c0
babdellghani <babdellghani@gmail.com>
2025-11-09 02:35:55 +0100

    feat: Update module name and version; enhance POS receipt configuration with print last receipt option

--- __manifest__.py
+++ __manifest__.py
@@ -1,20 +1,28 @@
 {
-    'name': 'POS Receipt Design & Print Last Receipt With Custom Templates',
-    'version': '18.0.1.0.4',
+    'name': 'Custom POS Receipt Design & Last Receipt',
+    'version': '18.0.1.0.0',
     'category': 'Point of Sale',
-    'summary': "POS Receipt, Receipt Design, POS Receipt Template, Print Last Receipt, Design "
-               "Report, Custom Receipt, POS Report, Customise Receipt, Odoo18, "
-               "Odoo Apps, Print Last Receipt, POS Custom Receipt",
-    'description': "Option to select the customised Receipts for each POS. So, "
-                   "we can easily updated the Receipt Design for better styles",
-                    "and also added the Print Last Receipt button in the POS interface to print ",
-                     "the last receipt with custom templates.",
+    'summary': 'Add button to print last receipt in POS',
+    'description': """
+        POS Print Last Receipt
+        ======================
+        This module adds a button in the POS interface to print the last receipt.
+        
+        Features:
+        ---------
+        * Print Last Receipt button in the action pad
+        * Automatically finds the last paid order
+        * Uses Odoo's default receipt template
+        * Shows notifications for success/errors
+    """,
     'author': 'Abdelghani X',
-    'company': 'Abdelghani X',
-    'maintainer': 'Abdelghani X',
-    'depends': ['base', 'point_of_sale'],
+    'website': 'https://www.AbdelghaniX.com',
+    'license': 'OPL-1',
+    'price': 29.99,
+    'currency': 'USD',
+    'images': ['static/description/banner.png'],
+    'depends': ['point_of_sale'],
     'data': [
-        'security/ir.model.access.csv',
         'data/pos_receipt_design1_data.xml',
         'data/pos_receipt_design2_data.xml',
         'views/pos_receipt_views.xml',
@@ -28,7 +36,6 @@
             'custom_receipts_for_pos/static/src/xml/print_last_receipt_button.xml',
         ],
     },
-    'images': ['static/description/banner.png'],
     'installable': True,
     'auto_install': False,
     'application': False
